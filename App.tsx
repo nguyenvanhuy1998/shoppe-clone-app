9b57ff47ecc9810cd6450895033e710daf759994
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-04-24 16:15:02 +0700

    Config axios and react query

--- App.tsx
+++ App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStackNavigator} from './src/navigator';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 const App = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <RootStackNavigator />
+        <QueryClientProvider client={queryClient}>
+          <RootStackNavigator />
+        </QueryClientProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
