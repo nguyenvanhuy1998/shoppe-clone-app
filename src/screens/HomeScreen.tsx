d144e4a5e6ed7a240a16d3853431e0c236f55b63
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-05-06 16:07:02 +0700

    Reverse Home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,44 +1,10 @@
-import React, {useRef} from 'react';
-import {Dimensions, Text, View} from 'react-native';
-import {ContainerComponent} from '../components';
-import Carousel from 'react-native-snap-carousel';
+import React from 'react';
+import {ContainerComponent, TextComponent} from '../components';
 
 const HomeScreen = () => {
-  const carouselRef = useRef(null);
-  const {width: viewportWidth} = Dimensions.get('window');
-
-  const data = [
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    // Add more items here
-  ];
-  const renderItem = ({item}) => (
-    <View
-      style={{
-        backgroundColor: 'white',
-        borderRadius: 5,
-        height: 250,
-        padding: 50,
-        marginLeft: 25,
-        marginRight: 25,
-      }}>
-      <Text>{item}</Text>
-    </View>
-  );
   return (
     <ContainerComponent type="noSafeArea">
-      <Carousel
-        ref={carouselRef}
-        data={data}
-        renderItem={renderItem}
-        sliderWidth={viewportWidth}
-        itemWidth={viewportWidth - 50}
-        loop={true}
-        autoplay={true}
-        autoplayDelay={500}
-        autoplayInterval={3000}
-      />
+      <TextComponent text="123qkwjekqwjekwj" />
     </ContainerComponent>
   );
 };
