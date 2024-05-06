281adccb2136f73c55fbaaf35cd58dda94e8e059
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-05-06 14:15:46 +0700

    Setup carousel and add patch package

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,13 @@
 export const logo = require('../assets/images/shopee-logo.png');
+export const banner01 = require('../assets/images/banner-01.jpg');
+export const banner02 = require('../assets/images/banner-02.jpg');
+export const banner03 = require('../assets/images/banner-03.png');
+export const banner04 = require('../assets/images/banner-04.jpg');
+
 export default {
   logo,
+  banner01,
+  banner02,
+  banner03,
+  banner04,
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useRef} from 'react';
+import {Dimensions, Text, View} from 'react-native';
 import {ContainerComponent} from '../components';
+import Carousel from 'react-native-snap-carousel';
 
 const HomeScreen = () => {
+  const carouselRef = useRef(null);
+  const {width: viewportWidth} = Dimensions.get('window');
+
+  const data = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    // Add more items here
+  ];
+  const renderItem = ({item}) => (
+    <View
+      style={{
+        backgroundColor: 'white',
+        borderRadius: 5,
+        height: 250,
+        padding: 50,
+        marginLeft: 25,
+        marginRight: 25,
+      }}>
+      <Text>{item}</Text>
+    </View>
+  );
   return (
     <ContainerComponent type="noSafeArea">
-      <Text>Home Screen</Text>
+      <Carousel
+        ref={carouselRef}
+        data={data}
+        renderItem={renderItem}
+        sliderWidth={viewportWidth}
+        itemWidth={viewportWidth - 50}
+        loop={true}
+        autoplay={true}
+        autoplayDelay={500}
+        autoplayInterval={3000}
+      />
     </ContainerComponent>
   );
 };
